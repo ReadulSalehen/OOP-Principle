ffbfd54b65ae4144d998ed8a4bef135bb4463107
Readul Salehen <102899309+ReadulSalehen@users.noreply.github.com>
2023-08-20 22:31:25 +0600

    Add files via upload
    initial commit

---
+++ Polymorphism/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+
+                      //Methode-Overriding
+    public class Animal
+    {
+                        //Constructor-Overloading
+        //public Animal()
+        //{
+                
+        //}
+
+        //public Animal(string name)
+        //{
+            
+        //}
+
+        //public Animal(string name, int id)
+        //{
+            
+        //}
+        public void PrintName()
+        {
+            Console.WriteLine("This is an animal");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public void PrintName()
+        {
+            Console.WriteLine("This is a cat");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void PrintName()
+        {
+            Console.WriteLine("This is a dog");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.PrintName();
+            Animal cat = new Cat();
+            cat.PrintName();
+            Dog d = new Dog();
+            d.PrintName();
+            Animal dog = new Dog();
+            dog.PrintName(); 
+        }
+
+
+                //Methode-Overloding
+
+        //public static int Sum(int x, int y)
+        //{
+        //    return x + y;
+        //}
+
+        //public static int Sum(int x, int y, int z)
+        //{
+        //    return x+y+z;
+        //}
+    }
+}
